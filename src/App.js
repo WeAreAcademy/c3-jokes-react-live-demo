9748661c28d325d3d22cda9154a089941c644cf3
Matthew Man <43723450+Matthew-Man@users.noreply.github.com>
2021-11-18 12:16:03 +0000

    Basic fetch added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  async function handleGetJoke() {
+    const result = await fetch("http://localhost:5000/jokes/random");
+    const joke = await result.json();
+    console.log("get my random joke: ", joke);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGetJoke}>Random Joke</button>
     </div>
   );
 }
