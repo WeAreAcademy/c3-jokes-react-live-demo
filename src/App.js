565ede4b356a2986c32770d702256ca1a0d54a85
Matthew Man <43723450+Matthew-Man@users.noreply.github.com>
2021-11-18 12:32:35 +0000

    Added search joke functionality and rendered results

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [allJokes, setAllJokes] = useState([]);
+
   async function handleGetJoke() {
     const result = await fetch("http://localhost:5000/jokes/random");
-    const joke = await result.json();
-    console.log("get my random joke: ", joke);
+    const jokes = await result.json();
+    setAllJokes(jokes);
   }
+
+  async function handleSearchJoke() {
+    const result = await fetch(
+      `http://localhost:5000/jokes/search?term=${searchTerm}`
+    );
+    const jokes = await result.json();
+    setAllJokes(jokes);
+  }
+
+  const jokesAsString = allJokes
+    .map((joke) => joke.setup + ": " + joke.punchline)
+    .join(",");
 
   return (
     <div className="App">
       <button onClick={handleGetJoke}>Random Joke</button>
+      <div style={{ margin: "20px" }}>
+        <input
+          placeholder="search here"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearchJoke}>
+          Search for jokes about {searchTerm}
+        </button>
+      </div>
+      {jokesAsString}
     </div>
   );
 }
